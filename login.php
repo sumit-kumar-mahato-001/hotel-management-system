05f157c51a6c5301b5a9f01d208c58326303fc65
Sumit Mahato <203450058+sumit-kumar-mahato-001@users.noreply.github.com>
2026-06-27 18:27:49 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,14 @@
-<?php // Login Page ?>
+<?php $pageTitle = "Login | Hotel Management System"; include "includes/header.php"; ?>
+
+<section class="auth">
+  <h1>User Login</h1>
+  <form class="form" onsubmit="demoSubmit(event)">
+    <input type="email" placeholder="Email Address" required>
+    <input type="password" placeholder="Password" required>
+    <button class="btn" type="submit">Login</button>
+    <p id="formMsg"></p>
+  </form>
+  <p>New user? <a href="register.php">Register here</a></p>
+</section>
+
+<?php include "includes/footer.php"; ?>
